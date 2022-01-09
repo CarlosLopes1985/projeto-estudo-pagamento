e4ee42ff7575db20305835b24cb366b4081afc28
Carlos-Lopes1985 <carlosslopes1985@hotmail.com>
2022-01-09 00:01:46 -0300

    primeiro commit

---
+++ app/src/main/java/com/estrelas/carrinho/pagamento/CarrinhopagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.estrelas.carrinho.pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhopagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrinhopagamentoApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/estrelas/carrinho/pagamento/CarrinhopagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estrelas.carrinho.pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrinhopagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
